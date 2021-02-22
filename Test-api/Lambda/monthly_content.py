ca1b9ec59b669b1b55eaae7951c333702edb0b36
Amelia Young <emilyjr1@naver.com>
2021-02-22 18:08:21 +0900

    Update files

--- Test-api/Lambda/monthly_content.py
+++ Test-api/Lambda/monthly_content.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-def monthly_content():
-    
-=======
-import requests
-from get_headers import GetHeader
-
-def monthly_content(keyword, start_date, end_date, time_unit): #검색 키워드, 시간 단위
-
->>>>>>> b8b78118e8555f273aeead5bfb8fd0a23fd404a2
+def monthly_content(encText, ):
